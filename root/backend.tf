e77c694360987990d799322bfd4ff6f67fc12d14
piyushsachdeva <piyush.sachdeva055@gmail.com>
2023-07-30 23:53:26 -0400

    removed cache

--- root/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tfstate-piyush-101"
-    key    = "backend/10weeksofcloudops-demo.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "remote-backend"
-  }
-}
